1b54786ce2652626e62596ed55e4305c8620efcb
SnoweTiger <snowetiger@gmail.com>
2025-09-29 21:50:51 +0300

    add static faces smile

---
+++ libs/robo_face.py
@@ -0,0 +1,205 @@
+import math
+from enum import Enum
+
+from libs.oled import SSD1306
+
+
+class MouthType(Enum):
+    neutral = 1
+    smile = 2
+    negative = 3
+
+
+class Mood(Enum):
+    neutral = 1
+    angry = 2
+    smile = 3
+    happy = 4
+    shocked = 5
+
+
+class RoboFace:
+    def __init__(self, oled: SSD1306, border: bool = False, color: int = 1):
+        self.oled = oled
+        self.cx = oled.width // 2
+        self.cy = oled.height // 2
+        self.border = border
+        self.color = color
+
+        self.radius = int(min(oled.width, oled.height) * 0.95) // 2
+
+        # Calc eys sizes
+        self.eye_offset_x = int(self.radius * 0.45)
+        self.eye_offset_y = int(self.radius * 0.35)
+        self.eye_radius = self.radius // 6
+        self.big_eye_scale = 2.0
+        self.small_eye_scale = 0.5
+        self.left_eye_scale = None
+        self.right_eye_scale = None
+
+        # Calc mouth sizes
+        self.smile_width = int(self.radius * 0.8)
+        self.smile_height = int(self.radius * 0.2)
+        self.mouth_y = self.cy + int(self.radius * 0.35)
+
+        # Calc eyebrow sizes
+        self.eyebrow_angle = 0
+        self.eyebrow_width = self.radius * 0.7
+
+        # Init face state
+        self.wink_left = False
+        self.wink_right = False
+        self.mouth_type = MouthType.neutral
+
+    def set_mood(self, mood: Mood) -> None:
+        self.mood = mood
+        match mood:
+            case Mood.smile:
+                self._set_smile()
+            case Mood.angry:
+                self._set_angry()
+            case Mood.happy:
+                self._set_happy()
+            case Mood.shocked:
+                self._set_shocked()
+            case Mood.neutral | _:
+                self._set_neutral()
+        self._draw_frame()
+
+    def _set_smile(self) -> None:
+        self.wink_left = False
+        self.wink_right = False
+        self.left_eye_scale = None
+        self.right_eye_scale = None
+        self.eyebrow_angle = 0
+        self.mouth_type = MouthType.smile
+
+    def _set_happy(self) -> None:
+        self.wink_left = True
+        self.wink_right = True
+        self.left_eye_scale = None
+        self.right_eye_scale = None
+        self.eyebrow_angle = 0
+        self.mouth_type = MouthType.smile
+
+    def _set_angry(self) -> None:
+        self.wink_left = False
+        self.wink_right = False
+        self.left_eye_scale = None
+        self.right_eye_scale = None
+        self.eyebrow_angle = math.pi / 8
+        self.mouth_type = MouthType.negative
+
+    def _set_shocked(self) -> None:
+        self.wink_left = False
+        self.wink_right = False
+        self.left_eye_scale = None
+        self.right_eye_scale = 2
+        self.eyebrow_angle = 0
+        self.mouth_type = MouthType.neutral
+
+    def _set_neutral(self) -> None:
+        self.wink_left = False
+        self.wink_right = False
+        self.left_eye_scale = None
+        self.right_eye_scale = None
+        self.eyebrow_angle = 0
+        self.mouth_type = MouthType.neutral
+
+    def _draw_frame(self) -> None:
+        oled = self.oled
+        oled.fill(0)
+
+        if self.border:
+            self.oled.circle(self.cx, self.cy, self.radius, 1)
+
+        # Left eye
+        if self.wink_left:
+            oled.hline(
+                self.cx - self.eye_offset_x - self.eye_radius,
+                self.cy - self.eye_offset_y,
+                self.eye_radius * 2,
+                self.color,
+            )
+        else:
+            scale = (
+                1
+                if self.left_eye_scale is None
+                else max(0.5, min(2.0, self.left_eye_scale))
+            )
+            oled.filled_circle(
+                self.cx - self.eye_offset_x,
+                self.cy - self.eye_offset_y,
+                int(self.eye_radius * scale),
+                self.color,
+            )
+
+        # Right eye
+        if self.wink_right:
+            oled.hline(
+                self.cx + self.eye_offset_x - self.eye_radius,
+                self.cy - self.eye_offset_y,
+                self.eye_radius * 2,
+                self.color,
+            )
+        else:
+            scale = (
+                1
+                if self.right_eye_scale is None
+                else max(0.5, min(2.0, float(self.right_eye_scale)))
+            )
+            oled.filled_circle(
+                self.cx + self.eye_offset_x,
+                self.cy - self.eye_offset_y,
+                int(self.eye_radius * scale),
+                self.color,
+            )
+
+        # Eyebrows
+        if self.eyebrow_angle != 0.0:
+            dx = int(math.cos(self.eyebrow_angle) * self.eyebrow_width)
+            dy = int(math.sin(self.eyebrow_angle) * self.eyebrow_width)
+
+            eyebrow_x_l = self.cx - self.eye_offset_x + int(self.radius * 0.05)
+            eyebrow_x_r = self.cx + self.eye_offset_x - int(self.radius * 0.05)
+            eyebrow_y = (
+                self.cy - self.eye_offset_y - self.eye_radius - int(self.radius * 0.1)
+            )
+            x0 = eyebrow_x_l - dx // 2
+            y0 = eyebrow_y - dy // 2
+            x1 = eyebrow_x_l + dx // 2
+            y1 = eyebrow_y + dy // 2
+            oled.line(x0, y0, x1, y1, 1)
+
+            x0 = eyebrow_x_r - dx // 2
+            y0 = eyebrow_y + dy // 2
+            x1 = eyebrow_x_r + dx // 2
+            y1 = eyebrow_y - dy // 2
+            oled.line(x0, y0, x1, y1, 1)
+
+        # Mouth
+        match self.mouth_type:
+            case MouthType.smile:
+                print("smile")
+                p0 = (self.cx - self.eye_radius * 2, self.mouth_y - self.smile_height)
+                p1 = (self.cx, self.mouth_y + self.smile_height)
+                p2 = (self.cx + self.eye_radius * 2, self.mouth_y - self.smile_height)
+                oled.quad_bezier(p0, p1, p2, offset_y=self.smile_height // 2)
+
+            case MouthType.negative:
+                print("negative")
+                p0 = (self.cx - self.eye_radius * 2, self.mouth_y + self.smile_height)
+                p1 = (self.cx, self.mouth_y - self.smile_height)
+                p2 = (self.cx + self.eye_radius * 2, self.mouth_y + self.smile_height)
+                oled.quad_bezier(p0, p1, p2, offset_y=-self.smile_height // 2)
+
+            case MouthType.neutral | _:
+                print("neutral")
+                oled.hline(
+                    self.cx - self.smile_width // 2,
+                    self.cy + int(self.radius * 0.35),
+                    self.smile_width,
+                    self.color,
+                )
+
+        oled.show()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
 from libs.oled import SSD1306
-
+from libs.robo_face import RoboFace, Mood
 
 DISPLAY_WIDTH = 128
 DISPLAY_HEIGHT = 64
@@ -9,13 +9,24 @@ I2C_BUS = 3  # for LuckFox Pico Pro/Max 3 by default -> /dev/i2c-3
 
 
 def main() -> None:
-    oled = SSD1306(DISPLAY_WIDTH, DISPLAY_HEIGHT, I2C_BUS, I2C_ADDR)
-
     try:
-        oled.fill(0)
-        oled.circle(oled.width // 2, oled.height // 2, oled.height // 2, 1)
-        oled.show()
-        time.sleep(0.8)
+        oled = SSD1306(DISPLAY_WIDTH, DISPLAY_HEIGHT, I2C_BUS, I2C_ADDR)
+        robo = RoboFace(oled)
+
+        robo.set_mood(Mood.neutral)
+        time.sleep(1)
+
+        robo.set_mood(Mood.shocked)
+        time.sleep(1)
+
+        robo.set_mood(Mood.angry)
+        time.sleep(1)
+
+        robo.set_mood(Mood.smile)
+        time.sleep(1)
+
+        robo.set_mood(Mood.happy)
+        time.sleep(1)
 
         oled.power_off()
     except Exception as e:
