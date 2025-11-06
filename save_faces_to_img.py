12af826ebc1e788e3707cf9ee3791eb21c38e87b
SnoweTiger <snowetiger@gmail.com>
2025-11-06 18:44:47 +0300

    add script o save all avaliable faces to images

---
+++ save_faces_to_img.py
@@ -0,0 +1,30 @@
+import time
+from libs.oled import SSD1306
+from libs.robo_face import RoboFace, Mood, Style
+
+DISPLAY_WIDTH = 128
+DISPLAY_HEIGHT = 64
+I2C_ADDR = 0x3C  # common addresses: 0x3C
+I2C_BUS = 3  # for LuckFox Pico Pro/Max 3 by default -> /dev/i2c-3
+
+
+def main() -> None:
+    try:
+        oled = SSD1306(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        robo = RoboFace(oled, animation_duration=0.8, style=Style.robo_quad)
+
+        for style in Style:
+            robo = RoboFace(oled, style=style)
+            robo.file_prefix = style.name
+
+            for mood in Mood:
+                robo.set_mood(mood)
+            time.sleep(0.1)
+
+        oled.power_off()
+    except Exception as e:
+        print(f"Exception {e}")
+
+
+if __name__ == "__main__":
+    main()
